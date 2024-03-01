c3f3a803e22287df0fb9cd2785c798e978c95784
muthu <muthuraja.s2022cce@sece.ac.in>
2024-03-01 10:59:42 +0530

    first push

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const app = express()
 const static =  express.static("static");
 app.use("/",static);
 
+app.get("/hi",(req,res)=>{
+   console.log(req.query);
+   res.json({
+     
+      name:req.query.name,
+      description:req.query.description,
+      i_date:req.query.i_date,
+      amount:req.query.amount
+
+
+
+
+
+   });
+   
+
+})
 // app.get('/', function (req, res) {
 //  // res.send('Hello World')
 //  res.json({
@@ -13,4 +30,5 @@ app.use("/",static);
 
 app.listen(3000,()=>{
    console.log("app runing");
+   console.log("hi everyone")
 });
